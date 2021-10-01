86affb3910d2cdbf0a7830b6e50a08832e0599d8
codewithnaren <91406174+codewithnaren@users.noreply.github.com>
2021-10-01 17:22:19 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const url = 'https://codewithnaren.github.io/socialandpoliticallife1/Polity6.pdf';
+const url = 'https://codewithnaren.github.io/PhysicsClass11Part2/PhysicsClass11Part2.pdf';
 
 
 let pdfDoc = null,
